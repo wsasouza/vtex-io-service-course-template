d53e5f707b3a82b82fe530490c2e33a8270291fe
Walter Santos de Andrade Souza <wsasouza@hotmail.com>
2022-07-09 20:13:56 -0300

    Services version final

--- node/index.ts
+++ node/index.ts
@@ -5,20 +5,25 @@ import {
   ParamsContext,
   RecorderState,
   method,
-} from '@vtex/api'
-import { Clients } from './clients'
-import { analytics } from './handlers/analytics'
+} from '@vtex/api';
+import { Clients } from './clients';
+import { productList } from './resolvers/products';
+import { analytics } from './handlers/analytics';
+import { updateLiveUsers } from './event/liveUsersUpdate';
+
+const THREE_SECONDS_MS = 3 * 1000;
+const CONCURRENCY = 10;
 
 // Create a LRU memory cache for the Status client.
 // The @vtex/api HttpClient respects Cache-Control headers and uses the provided cache.
-const memoryCache = new LRUCache<string, any>({ max: 5000 })
-metrics.trackCache('status', memoryCache)
+const memoryCache = new LRUCache<string, any>({ max: 5000 });
+metrics.trackCache('status', memoryCache);
 
 declare global {
-  type Context = ServiceContext<Clients, State>
+  type Context = ServiceContext<Clients, State>;
 
   interface State extends RecorderState {
-    code: number
+    code: number;
   }
 }
 
@@ -30,6 +35,24 @@ export default new Service<Clients, State, ParamsContext>({
         retries: 2,
         timeout: 10000,
       },
+      events: {
+        exponentialTimeoutCoefficient: 2,
+        exponentialBackoffCoefficient: 2,
+        initialBackoffDelay: 50,
+        retries: 1,
+        timeout: THREE_SECONDS_MS,
+        concurrency: CONCURRENCY,
+      },
+    },
+  },
+  events: {
+    liveUsersUpdate: updateLiveUsers,
+  },
+  graphql: {
+    resolvers: {
+      Query: {
+        productList,
+      },
     },
   },
   routes: {
@@ -37,4 +60,4 @@ export default new Service<Clients, State, ParamsContext>({
       GET: [analytics],
     }),
   },
-})
+});
